37da0c83b455a6209f79ddd6c3c9a025bb3b5f38
hanier10 <hsmp16@gmail.com>
2022-12-08 15:15:45 -0600

    Tipos de datos y TemplateStrings

---
+++ src/bases/01-types.ts
@@ -0,0 +1,15 @@
+export let name = "Hanier";
+export const age = 26;
+export const isValid: boolean = true;
+
+export const templateString = `
+Aqui puedo
+escribir
+lo que desee y nadie dirá nada
+puedo decir que ${1 + 1}
+tambien puedo traerme el nombre de ${name} y
+otras cosas interesantes, usar templateString
+implementando BackTicks es sorprendente
+`;
+
+console.log(templateString);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
+import { name } from "./bases/01-types";
+import { age } from "./bases/01-types";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -10,7 +12,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Hola soy ${name} y estoy aprendiendo + TypeScript, tengo la edad de ${age}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
