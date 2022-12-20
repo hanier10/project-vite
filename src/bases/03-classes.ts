ea0dd1da200f5dd021ecf3786a6daeb6b465b7a8
hanier10 <hsmp16@gmail.com>
2022-12-19 23:56:48 -0600

    getters, metodos y this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,5 +1,25 @@
 export class Pokemon {
-  constructor(public readonly id: number, public name: string) {}
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`;
+  }
+
+  constructor(
+    public readonly id: number,
+    public name: string // public imageUrl: string
+  ) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name} ${this.name}`);
+  }
 }
 
 export const charmander: Pokemon = new Pokemon(4, "charmander");
+
+console.log(charmander, charmander.imageUrl);
+
+charmander.scream();
+charmander.speak();
