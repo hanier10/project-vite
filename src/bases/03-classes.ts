8af72a24cd734adc3c4f1e05d2400792d48541ec
hanier10 <hsmp16@gmail.com>
2022-12-26 00:50:05 -0600

    asignando tipo de dato mediante axios usando interfaces

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import {
+  Move,
+  PokeapiResponse,
+} from "../interfaces/pokeapi-response.interface";
 export class Pokemon {
   get imageUrl(): string {
     return `https://pokemon.com/${this.id}.jpg`;
@@ -16,8 +20,10 @@ export class Pokemon {
   speak() {
     console.log(`${this.name} ${this.name}`);
   }
-  async getMoves() {
-    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeapiResponse>(
+      "https://pokeapi.co/api/v2/pokemon/4"
+    );
     console.log(data.moves);
     // return answer;
     return data.moves;
