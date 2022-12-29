5b83322bb7049cd4d2985ecb7c5eeda24cf654c8
hanier10 <hsmp16@gmail.com>
2022-12-28 19:00:37 -0600

    creando decoradores personalizados

---
+++ src/bases/05-decorators.ts
@@ -0,0 +1,35 @@
+export class NewPokemon {
+  constructor(public readonly id: number, public name: string) {}
+
+  scream() {
+    console.log("No quiero!!");
+  }
+
+  speak() {
+    console.log("No quiero hablar");
+  }
+}
+
+const MiDecorador = () => {
+  return (target: Function) => {
+    return NewPokemon;
+  };
+};
+
+@MiDecorador()
+export class Pokemon {
+  constructor(public readonly id: number, public name: string) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}!`);
+  }
+}
+
+export const charmander = new Pokemon(4, "Charmander");
+
+charmander.scream();
+charmander.speak();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
-import { charmander } from "./bases/04-injection";
+import { charmander } from "./bases/05-decorators";
 // import { bulbasaur, pokemons } from "./bases/02-objects";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
@@ -12,7 +12,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Hello ${charmander.name}!</h1>
+    <h1>Hello ${charmander.name} ${charmander.id}!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
